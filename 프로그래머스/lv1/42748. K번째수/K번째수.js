f2b1674db2dc54d16ce45f0e20083431cac54663
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-11-27 20:42:10 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,9 +1,7 @@
 function solution(array, commands) {
     const answer = [];
     commands.forEach(item => {
-        const i = item[0];
-        const j = item[1];
-        const k = item[2];
+        const [i, j, k] = item;
         const sliceArr = array.slice(i-1, j).sort((a, b) => a - b);
         answer.push(sliceArr[k-1]);
     });
