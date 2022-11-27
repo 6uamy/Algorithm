e4419d4dd81eb8f7284de92d797a3fbea6fe2782
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-11-27 21:40:02 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    let arr = [/zero/ig,/one/ig,/two/ig,/three/ig,/four/ig,/five/ig,/six/ig,/seven/ig,
+              /eight/ig,/nine/ig];
+    
+    for (let i=0; i<10; i++) {
+    let answer = s.replaceAll(arr[i],i);
+    s = answer;
+    }
+    return Number(s);
+}
