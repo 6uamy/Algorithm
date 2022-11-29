6a8e32cf59b9b6200b57392e9c8fe4bbe972f8e7
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-11-29 10:17:21 +0900

    [level 1] Title: 소수 찾기, Time: 218.39 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,14 @@
+function isPrime(number) {
+    for (let i=2; i<=Math.sqrt(number); i++) {
+        if (number % i === 0) return false;
+    }
+    return true;
+}
+
+function solution(n) {  
+    let answer = 0;
+    for (let i=2; i<=n; i++) {
+        if (isPrime(i)) answer++;
+    }
+    return answer;  
+}
