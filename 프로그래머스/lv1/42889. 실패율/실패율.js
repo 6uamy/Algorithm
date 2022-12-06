78548727051471a9d5ec11bbb0843c23f57861b6
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-12-06 10:11:52 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,25 +1,36 @@
 function solution(N, stages) {
-    const answer = [];
-    const result = [];
-    let greeter = stages.length;
-    for (let i=1; i<=N; i++) {
-        let fail = 0;
-        for (let j=0; j<stages.length; j++) {
-            if (stages[j] <= i) {
-                stages[j] = N+1
-                fail++;
-            }
-        }
-        answer.push(fail / greeter);
-        greeter -= fail;
-    }
+/*   기존 풀이 방식  
+     const answer = [];
+     const result = [];
+     let greeter = stages.length;
+     for (let i=1; i<=N; i++) {
+         let fail = 0;
+         for (let j=0; j<stages.length; j++) {
+             if (stages[j] <= i) {
+                 stages[j] = N+1
+                 fail++;
+             }
+         }
+         answer.push(fail / greeter);
+         greeter -= fail;
+     }
+  
+     let cnt = 1;
+     for (let i=0; i<answer.length; i++) {
+         result.push([cnt, answer[i]]);
+         cnt++;
+     }
+     result.sort((a, b) => b[1] - a[1]);
     
-    let cnt = 1;
-    for (let i=0; i<answer.length; i++) {
-        result.push([cnt, answer[i]]);
-        cnt++;
-    }
-    result.sort((a, b) => b[1] - a[1]);
+     return result.map(x => x[0]); */ 
     
-    return result.map(x => x[0]);
+    // filter 사용
+    let result = [];
+    for(let i=1; i<=N; i++){
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        result.push([i, curr/reach]);
+    }
+    result.sort((a,b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
 }
