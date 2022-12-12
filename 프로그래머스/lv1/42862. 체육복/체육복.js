759549e4dd6f9bd3b4417d6079ce89845fd05c36
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-12-12 20:29:03 +0900

    [level 1] Title: 체육복, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -5,7 +5,7 @@ function solution(n, lost, reserve) {
     
     for (let i = 0; i < lostLength ; i ++) {
         if (reserve.includes(lost[i])) {
-            reserve = reserve.filter((ele) => ele !== lost[i])
+            reserve = reserve.filter((ele) => ele !== lost[i]);
             answer += 1;
         } else {
             tmp.push(lost[i]);
@@ -17,11 +17,11 @@ function solution(n, lost, reserve) {
     for (let i = 0 ; i < lostLength ; i ++) {
         if (reserve.includes(lost[i] - 1 )) {
             answer += 1 ;
-            reserve = reserve.filter ((ele) => ele !== lost[i] - 1 )
+            reserve = reserve.filter ((ele) => ele !== lost[i] - 1 );
         }
         else if (reserve.includes(lost[i] + 1)) {
             answer += 1;
-            reserve = reserve.filter((ele) => ele !== lost[i] + 1)
+            reserve = reserve.filter((ele) => ele !== lost[i] + 1);
         }
     }
     
