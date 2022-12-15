9e76fe182aef6cc118d7eac816e53ab5b59cc4a2
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-12-15 23:06:30 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.77 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -4,19 +4,17 @@ function solution(board, moves) {
     
     for (let i = 0; i < moves.length; i++) {
         for (let j = 0; j < board.length; j++) {
-            if (board[j][moves[i] - 1] === 0) {
-                continue;
+            if (board[j][moves[i] - 1] === 0) continue;
+            
+            if (bucket[bucket.length - 1] === board[j][moves[i] - 1]) {
+                bucket.pop();
+                board[j][moves[i] - 1] = 0;
+                answer += 2;
+                break;
             } else {
-                if (bucket[bucket.length - 1] === board[j][moves[i] - 1]) {
-                    bucket.pop();
-                    board[j][moves[i] - 1] = 0;
-                    answer += 2;
-                    break;
-                } else {
-                    bucket.push(board[j][moves[i] - 1]);
-                    board[j][moves[i] - 1] = 0;
-                    break;
-                }
+                bucket.push(board[j][moves[i] - 1]);
+                board[j][moves[i] - 1] = 0;
+                break;
             }
         }
     }
