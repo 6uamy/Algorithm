857d452c9a0c620a3a2ba891034f501fcd0d6fd2
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2023-01-08 22:11:10 +0900

    [Bronze I] Title: 접두사 배열, Time: 220 ms, Memory: 24292 KB -BaekjoonHub

--- 백준/Bronze/13322. 접두사 배열/접두사 배열.js
+++ 백준/Bronze/13322. 접두사 배열/접두사 배열.js
@@ -2,15 +2,10 @@ const fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim();
 
 function solution(str) {
-    const arr = [];
     const answer = [];
     for (let i = 0; i < str.length; i++) {
-        arr.push(str.slice(0, i + 1));
+        answer.push(i);
     }
-
-    arr.forEach(item => {
-        answer.push(arr.indexOf(item));
-    });
 
     return answer.join('\n');
 }
