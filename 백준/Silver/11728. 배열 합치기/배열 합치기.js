b121da16307ff22feb285b75ae15022d09bbbf78
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2023-01-09 12:45:44 +0900

    [Silver V] Title: 배열 합치기, Time: 1728 ms, Memory: 366668 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.js
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.js
@@ -1,16 +1,6 @@
 const fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [aN, bN] = input.shift().split(' ');
-const arrA = input.shift().trim().split(' ');
-const arrB = input.shift().trim().split(' ');
-const answer = [];
+const newArr = [...input[1].split(' '), ...input[2].split(' ')];
 
-for (let i = 0; i < aN; i++) {
-    answer.push(arrA[i]);
-}
-for (let i = 0; i < bN; i++) {
-    answer.push(arrB[i]);
-}
-
-console.log(answer.sort((a, b) => a - b).join(' '));
+console.log(newArr.sort((a, b) => a - b).join(' '));
