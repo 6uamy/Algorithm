1fe3bc0b249ed4cb640eb2fc168257f8aa6dd033
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2023-01-23 23:32:26 +0900

    [Silver III] Title: N과 M (3), Time: 804 ms, Memory: 151276 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -0,0 +1,21 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const [N, M] = require('fs').readFileSync(filePath).toString().trim().split(' ').map(x=>+x);
+const answer = [];
+const ans_list = [];
+
+function solution(K) {
+  if(K === M) {
+    ans_list.push(answer.join(' '));
+    return;
+  }
+
+  for(let i=1; i<=N; i++) {
+    answer[K] = i;
+    solution(K+1);
+  }
+
+  return answer;
+}
+
+solution(0);
+console.log(ans_list.join('\n'))
