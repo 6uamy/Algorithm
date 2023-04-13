9d457a56d49a35d3e02f877056b2d06fc74b153f
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2023-04-13 23:57:43 +0900

    [unrated] Title: 추억 점수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,21 +1,20 @@
 function solution(name, yearning, photo) {
-    var answer = [];
-    let chart = {};
-    name.forEach((x,i)=>{
-      chart[x]=yearning[i]
-    })
- 
-  answer = photo.map(p=>{
-    let sum = 0;
-    p.forEach(n=>{
-      if(chart[n]){
-        sum+=chart[n]
-      }else{
-        sum+=0
-      }
-      
-    })
-    return sum
-  })
+    const answer = [];
+    const hash = new Map();
+    
+    name.forEach((item, index) => {
+        hash.set(item, yearning[index]);
+    });
+    
+    for(let i = 0; i < photo.length; i++) {
+        let count = 0;
+        for(let j = 0; j < photo[i].length; j++) {
+            const score = hash.get(photo[i][j]);
+            if(!score) continue;
+            count += score;
+        }
+        answer.push(count);
+    }
+    
     return answer;
 }
