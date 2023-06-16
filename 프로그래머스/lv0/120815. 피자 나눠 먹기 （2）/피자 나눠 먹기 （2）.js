5841f94e1032c88e96353a184574eca2e6039d4f
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2023-06-16 17:19:11 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let pizza = 1;
+    
+    while(true) {
+        if ((6 * pizza) % n === 0) {
+            return pizza;
+        } else {
+            pizza++;   
+        }
+    }
+}
