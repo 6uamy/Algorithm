f0b3dce24ad4781c270cdeb6a505343ca38ecded
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2023-06-16 22:56:39 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,11 +1,5 @@
 function solution(numbers, direction) {
-    if (direction === 'right') {
-        const num = numbers.pop();
-        numbers.unshift(num);
-    } else {
-        const num = numbers.shift();
-        numbers.push(num);
-    }
+    direction === 'right' ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift());
     
     return numbers;
 }
