3be9ca6ad94cd239fad136041cbc1d15a86708be
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2023-06-18 18:47:17 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,10 +1,5 @@
 function solution(emergency) {
     const emergencyHigh = [...emergency].sort((a, b) => b - a);
-    const answer = [];
     
-    for (let i = 0; i < emergency.length; i++) {
-        answer.push((emergencyHigh.indexOf(emergency[i])) + 1);
-    }
-    
-    return answer;
+    return emergency.map(x => emergencyHigh.indexOf(x) + 1);
 }
